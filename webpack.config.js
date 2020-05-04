ba5e65ba2d3d7e97735508ec34e4555268ecbcc6
certilremy <certilremy@gmail.com>
2020-05-04 12:52:18 -0400

    styled the header

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   watch: true,
   entry: './src/index.js',
   output: {
